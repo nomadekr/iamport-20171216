b14ab24f636c3d313af1fb7d791432f10de9dbad
Chinseok Lee <allieuslee@gmail.com>
2017-12-16 15:01:05 +0900

    MEDIA 설정

--- askdjango/urls.py
+++ askdjango/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -21,3 +23,6 @@ urlpatterns = [
     url(r'^accounts/', include('accounts.urls')),
     url(r'^shop/', include('shop.urls', namespace='shop')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
