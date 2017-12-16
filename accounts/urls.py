92f95b049dc78b4323e972260ffbb532fff789d1
Chinseok Lee <allieuslee@gmail.com>
2017-12-16 14:41:48 +0900

    프로필 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login
+from .views import profile
 
 urlpatterns = [
     url(r'^login/$', login, name='login', kwargs={
         'template_name': 'accounts/login.html'
     }),
+    url(r'^profile/$', profile, name='profile'),
 ]
 
