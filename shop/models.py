d3dc069f994ecbe20b1d4808403fcc27fcaa2562
Chinseok Lee <me@nomade.kr>
2017-12-16 16:16:15 +0900

    shop.Order모델에 merchant_uid/imp_uid/status 필드 추가 및 마이그레이션

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from django.conf import settings
 from django.db import models
 
@@ -15,7 +16,19 @@ class Item(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     item = models.ForeignKey(Item)
+    merchant_uid = models.UUIDField(default=uuid4, editable=False)
+    imp_uid = models.CharField(max_length=100, blank=True)
     name = models.CharField(max_length=100, verbose_name='상품명')
+    status = models.CharField(
+        max_length=9,
+        choices=(
+            ('ready',     '미결제'),
+            ('paid',      '결제완료'),
+            ('cancelled', '결제취소'),
+            ('failed',    '결제실패'),
+        ),
+        default='ready',
+        db_index=True)
     amount = models.PositiveIntegerField(verbose_name='결제금액')
 
     class Meta:
