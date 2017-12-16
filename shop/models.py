a8c1eb656001ad17fe9847cf6de56dce9a2f2e87
Chinseok Lee <allieuslee@gmail.com>
2017-12-16 15:03:38 +0900

    shop.Item 모델 추가 및 admin 등록

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, verbose_name='상품명')
+    desc = models.TextField(blank=True)
+    amount = models.PositiveIntegerField(verbose_name='결제금액')
+    photo = models.ImageField()
+
