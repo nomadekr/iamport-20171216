f6a8f3d029ec825ac5b5486a89114bcc6e3ede6a
Chinseok Lee <allieuslee@gmail.com>
2017-12-16 14:58:18 +0900

    shop앱을 생성/등록/URL생성

--- askdjango/urls.py
+++ askdjango/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
+    url(r'^shop/', include('shop.urls', namespace='shop')),
 ]
