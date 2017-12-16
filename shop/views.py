f79b3675dd45ed4a2fc51c98d1d05c65152e5816
Chinseok Lee <allieuslee@gmail.com>
2017-12-16 15:37:37 +0900

    상품 간단검색 구현

--- shop/views.py
+++ shop/views.py
@@ -3,5 +3,20 @@ from django.views.generic import ListView
 from .models import Item
 
 
-index = ListView.as_view(model=Item)
+class ItemListView(ListView):
+    model = Item
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        self.q = self.request.GET.get('q', '')
+        if self.q:
+            qs = qs.filter(name__icontains=self.q)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.q
+        return context
+
+index = ItemListView.as_view()
 
